3d61ef4779ce51e05f2535f148fd2af3d9b879dd
Alisher <alisktl@gmail.com>
2015-11-09 17:49:55 +0600

    1. Changing expectations: Cat Clicker, two cats

--- 1. Changing Expectations/Cat Clicker/js/main.js
+++ 1. Changing Expectations/Cat Clicker/js/main.js
@@ -1,11 +1,17 @@
 var clicks = 0;
+var clicks2 = 0;
 
 $(function() {
 	var $catClickCounter = $('#counter');
+	var $catClickCounter2 = $('#counter2');
 
 	$('#cat').click(function(e) {
 		clicks++;
-		console.log(clicks);
 		$catClickCounter.text(clicks);
+	});
+
+	$('#cat2').click(function(e) {
+		clicks2++;
+		$catClickCounter2.text(clicks2);
 	});
 });
